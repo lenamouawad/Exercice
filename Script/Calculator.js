33a6766d5800a97af98c7c4d0e510eddeb2ff3a7
Lena Mouawad <lena.mouawad@hotmail.com>
2021-10-08 15:50:41 +0200

    CalculatriceAfficher

--- Script/Calculator.js
+++ Script/Calculator.js
@@ -1,26 +1,32 @@
-let afficher = () => {
-        document.getElementById("affichage").innerHTML += "1"
+let afficher = (something) => {
+    if (something === "=") {
+        document.getElementById("affichage").innerHTML = "" // A changer
+    } else {
+        document.getElementById("affichage").innerHTML += something
     }
-    /*
-    let read = require("readline-sync")
+}
 
-    let operateur = read.question("Entrer l'operateur")
-    let operande1 = read.questionInt("Entrer le permier nombre")
-    let operande2 = read.questionInt("Entrer le second nombre")
 
-    function addition(op1, operateur, op2) {
-        var result;
-        if (operateur == '+') {
-            result = op1 + op2;
-        } else if (operateur == '-') {
-            result = op1 - op2;
-        } else if (operateur == '*') {
-            result = op1 * op2;
-        } else if (operateur == '/') {
-            result = op1 / op2;
-        }
-        console.log(`[ ${op1}, "${operateur}", ${op2}] = ${result}`)
+/*
+let read = require("readline-sync")
 
+let operateur = read.question("Entrer l'operateur")
+let operande1 = read.questionInt("Entrer le permier nombre")
+let operande2 = read.questionInt("Entrer le second nombre")
+
+function addition(op1, operateur, op2) {
+    var result;
+    if (operateur == '+') {
+        result = op1 + op2;
+    } else if (operateur == '-') {
+        result = op1 - op2;
+    } else if (operateur == '*') {
+        result = op1 * op2;
+    } else if (operateur == '/') {
+        result = op1 / op2;
     }
+    console.log(`[ ${op1}, "${operateur}", ${op2}] = ${result}`)
 
-    addition(operande1, operateur, operande2)*/
+}
+
+addition(operande1, operateur, operande2)*/
