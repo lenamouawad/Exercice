4ccc24b0676a7fa51b4f82119cb4d61ec006b613
Lena Mouawad <lena.mouawad@hotmail.com>
2021-10-08 16:55:05 +0200

    AddedCEAndBackspace

--- Script/Calculator.js
+++ Script/Calculator.js
@@ -1,5 +1,11 @@
 let afficher = (something) => {
-    document.getElementById("affichage").innerHTML += something
+    if (something == erase) {
+        document.getElementById("affichage").innerHTML = " "
+    } else if (something == backspace) {
+        document.getElementById("affichage").innerHTML = document.getElementById("affichage").innerHTML.slice(0, document.getElementById("affichage").innerHTML.length - 1)
+    } else {
+        document.getElementById("affichage").innerHTML += something
+    }
 }
 
 let calcul = () => {
