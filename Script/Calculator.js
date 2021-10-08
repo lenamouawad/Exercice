c7fab9b89f21f0663bf4a8a0bcaa5453113af63a
Lena Mouawad <lena.mouawad@hotmail.com>
2021-10-08 16:30:36 +0200

    CalculatriceCalcul

--- Script/Calculator.js
+++ Script/Calculator.js
@@ -1,32 +1,9 @@
 let afficher = (something) => {
-    if (something === "=") {
-        document.getElementById("affichage").innerHTML = "" // A changer
-    } else {
-        document.getElementById("affichage").innerHTML += something
-    }
+    document.getElementById("affichage").innerHTML += something
 }
 
-
-/*
-let read = require("readline-sync")
-
-let operateur = read.question("Entrer l'operateur")
-let operande1 = read.questionInt("Entrer le permier nombre")
-let operande2 = read.questionInt("Entrer le second nombre")
-
-function addition(op1, operateur, op2) {
-    var result;
-    if (operateur == '+') {
-        result = op1 + op2;
-    } else if (operateur == '-') {
-        result = op1 - op2;
-    } else if (operateur == '*') {
-        result = op1 * op2;
-    } else if (operateur == '/') {
-        result = op1 / op2;
-    }
-    console.log(`[ ${op1}, "${operateur}", ${op2}] = ${result}`)
-
+let calcul = () => {
+    let affiche = document.getElementById("affichage").innerHTML
+    console.log(eval(affiche))
+    document.getElementById("affichage").innerHTML = eval(affiche)
 }
-
-addition(operande1, operateur, operande2)*/
