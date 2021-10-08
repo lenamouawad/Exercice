e29ecd60320dc18b8f682ad7cdf3c3b29fae4dce
Lena Mouawad <lena.mouawad@hotmail.com>
2021-10-08 15:25:17 +0200

    CalculatriceBoutons

---
+++ Script/Calculator.js
@@ -0,0 +1,26 @@
+let afficher = () => {
+        document.getElementById("affichage").innerHTML += "1"
+    }
+    /*
+    let read = require("readline-sync")
+
+    let operateur = read.question("Entrer l'operateur")
+    let operande1 = read.questionInt("Entrer le permier nombre")
+    let operande2 = read.questionInt("Entrer le second nombre")
+
+    function addition(op1, operateur, op2) {
+        var result;
+        if (operateur == '+') {
+            result = op1 + op2;
+        } else if (operateur == '-') {
+            result = op1 - op2;
+        } else if (operateur == '*') {
+            result = op1 * op2;
+        } else if (operateur == '/') {
+            result = op1 / op2;
+        }
+        console.log(`[ ${op1}, "${operateur}", ${op2}] = ${result}`)
+
+    }
+
+    addition(operande1, operateur, operande2)*/
